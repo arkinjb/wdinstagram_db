9254e4b7f3b254b88ba7ed5c54715256a4acb5e8
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-27 17:30:11 -0400

    ported instagram to rails

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,37 @@
+class EntriesController < ApplicationController
+
+  def index
+    @entries = Entry.all
+  end
+
+  def new
+  end
+
+  def create
+    @entry = Entry.new(params[:entry])
+    @entry.save
+    redirect_to ("/entries/#{@entry.id}")
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    @entry.update(params[:entry])
+    redirect_to ("/entries/#{@entry.id}")
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to ("/entries")
+  end
+
+
+end
