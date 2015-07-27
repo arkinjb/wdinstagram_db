dcab2770824201485a2a5a64714a1c2ac35650dd
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-27 16:35:47 -0400

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Entry.destroy_all
+
+gram_one = Entry.create( name: "Adrian", photo_url: "http://www.deism.com/images/Einstein_laughing.jpeg", date_taken: "January 10, 2000" )
+gram_two = Entry.create( name: "Jimmy", photo_url: "https://www.timeshighereducation.co.uk/sites/default/files/Pictures/web/x/y/e/The_Pick_120112.jpg", date_taken: "February 29th, 2002" )
+gram_three = Entry.create( name: "Adrian", photo_url: "http://www.fillmurray.com/200/200", date_taken: "January 10, 2015" )
